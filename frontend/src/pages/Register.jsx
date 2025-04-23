920ed885c20a0a49d2114a0f8ea458cbcf0c3422
znarfieeee <watsszap23@gmail.com>
2025-04-23 09:57:43 +0800

    Register Form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+
     const handleSubmit = async event => {
         event.preventDefault()
         const formData = new FormData(event.target)
